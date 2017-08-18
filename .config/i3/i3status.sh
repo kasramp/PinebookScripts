643d73029f05af4740a343a2daf73a170f1f226f
kasramp <kasra_mp@yahoo.com>
2017-08-18 22:49:52 +0800

    Fix battery status in i3

---
+++ .config/i3/i3status.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+charging="Charging"
+battery_status=""
+
+update_battery_status() {
+  percentage=$(upower -i /org/freedesktop/UPower/devices/battery_battery | grep -E "percentage" | sed s/"percentage:"// | tr -d "[:space:]")
+  state=$(upower -i /org/freedesktop/UPower/devices/battery_battery | grep -E "state" | sed s/"state:"// | tr -d "[:space:]")
+  state=$(echo "$state" | sed "s/.*/\u&/")
+  if [ "$state" = "$charging" ]
+  then
+
+    battery_status=$(echo "CHR ⚡: $percentage $state")
+
+  else
+
+    battery_status=$(echo "BAT ⏳: $percentage $state")
+  fi
+}
+
+i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && update_battery_status && while :
+do
+  read line
+  update_battery_status
+  echo ",[{\"full_text\":\"${battery_status}\" },${line#,\[}" || exit 1
+done)
